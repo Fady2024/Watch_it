09b4f24498c6c3a7efd9417833df86c00aa9be1f
Mahmoud5881 <158474401+Mahmoud5881@users.noreply.github.com>
2024-11-30 18:54:53 +0200

    Show.java

--- src/main/java/com/example/fms_market/Show.java
+++ src/main/java/com/example/fms_market/Show.java
@@ -16,16 +16,16 @@ public abstract class Show {
     private int id;
     private String title;
     private Date release_date;
-    private Time duration;
+    private int duration;
   //  private Cast[] cast;
     private String[] genres;
   //  private Director director;
     private String[] language;
-    private float imdb_score;
+    private double imdb_score;
     private String country;
     private long budget;
     private String poster;
-    private String type;
+    private String category;
     private String description; // New field for show description
 
     public void setId(int id) {
@@ -52,11 +52,11 @@ public abstract class Show {
         return release_date;
     }
 
-    public void setDuration(Time duration) {
+    public void setDuration(int duration) {
         this.duration = duration;
     }
 
-    public Time getDuration() {
+    public int getDuration() {
         return duration;
     }
 
@@ -92,11 +92,11 @@ public abstract class Show {
         return language;
     }
 
-    public void setImdb_score(float imdb_score) {
+    public void setImdb_score(double imdb_score) {
         this.imdb_score = imdb_score;
     }
 
-    public float getImdb_score() {
+    public double getImdb_score() {
         return imdb_score;
     }
 
@@ -124,8 +124,8 @@ public abstract class Show {
         return poster;
     }
 
-    public void setType(String category) {
-        this.type = category;
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public String getType() {
