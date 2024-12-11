8f468cc0677bc96ce63cc12755b12e378721946c
Fady Gerges Kodsy <fadygerges2023@gmail.com>
2024-12-11 21:18:56 +0200

    Update Code

--- src/main/java/com/example/fms_market/HelloApplication.java
+++ src/main/java/com/example/fms_market/HelloApplication.java
@@ -2,28 +2,35 @@ package com.example.fms_market;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.apache.commons.math3.analysis.function.Add;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) {
-        try {
-            // Load data at the beginning
-            DataManager.loadData();
+    public void start(Stage stage) throws IOException {
+        // Load data at the beginning
+        DataManager.loadData();
 
-            new AddShow(new User("few","feewf","Fwefw","efwfwe","fwefwe","fwefew"),stage);
+        // Initialize the UI
+        new LoginPageFX(stage);
+        new AddShow(new User("few", "feewf", "Fwefw", "efwfwe", "fwefwe", "fwefew"), stage);
 
-            // Save data at the end
-            DataManager.saveData();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Save data at the end
+        DataManager.saveData();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void init() throws Exception {
+        SubscriptionManager.readSubscriptions();
+    }
+
+    @Override
+    public void stop() {
+        SubscriptionManager.writeSubscriptions();
     }
 }
