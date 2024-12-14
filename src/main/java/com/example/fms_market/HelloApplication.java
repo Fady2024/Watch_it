549d74e02dd2c4698a54c8d4be7d184aef2c065e
Fady Gerges Kodsy <fadygerges2023@gmail.com>
2024-12-14 04:30:27 +0200

    Update Code

--- src/main/java/com/example/fms_market/HelloApplication.java
+++ src/main/java/com/example/fms_market/HelloApplication.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
 
     @Override
-    public void init() throws Exception {
+    public void init() {
         SubscriptionManager.readSubscriptions();
     }
 
